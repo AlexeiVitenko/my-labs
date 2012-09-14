4200061e5dd493700c1727ad60888c4edeeb7ced
AlexeiVitenko@gmail.com <AlexeiVitenko@gmail.com@8d78331f-9e9b-e9d6-d8e1-e5db5a9c904c>
2012-09-14 20:21:59 +0000

    Histogram + dialog

--- ADB/ADB1/src/by/bsuir/histogram/Histogram.java
+++ ADB/ADB1/src/by/bsuir/histogram/Histogram.java
@@ -1,34 +1,51 @@
 package by.bsuir.histogram;
 
+import static by.bsuir.histogram.HistogramsConstants.LEFT_PADDING;
+import static by.bsuir.histogram.HistogramsConstants.X_PADDING;
+
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.util.Log;
 
 public class Histogram {
+	private Point mSize;
 	private List<Point> mValues;
 	private float mScaleX;
 	private float mScaleY;
 	private int mDelta;
 	private int mPixelsForDelta;
 	private Rect mDisplayRect;
+	private int maxSize;
+	private float mDensity;
 
-	public Histogram(Rect displayRect, List<Point> values) {
+	public Histogram(Rect displayRect, List<Point> values, float density) {
 		mValues = values;
+		maxSize = Math.max(displayRect.bottom, displayRect.right);
+		mDensity = density;
+		calculateSize();
 	}
 
 	// TODO add check for empty list
-	private void calculateDeltas() {
+	private void calculateSize() {
+		calculateWidth();
+	}
+
+	private void calculateHeight() {
 		Collections.sort(mValues, new Comparator<Point>() {
 
 			@Override
 			public int compare(Point lhs, Point rhs) {
-				return new Integer(lhs.y).compareTo(rhs.y);
+				return lhs.y > rhs.y ? 1 : lhs.y == rhs.y ? 0 : -1;
 			}
 		});
+
 		int nextDelta = Integer.MAX_VALUE;
 		int minDelta = Integer.MAX_VALUE;
 		for (int i = 1; i < mValues.size(); i++) {
@@ -38,9 +55,25 @@ public class Histogram {
 				minDelta = temp;
 			}
 		}
-		int height = mValues.get(mValues.size() - 1).y - mValues.get(0).y;
-		while ((height) * 2 + 50 < mDisplayRect.bottom) {
-			height *= 2;
+	}
+
+	private void calculateWidth() {
+		List<Point> values = new ArrayList<Point>(mValues);
+		Collections.sort(values, new Comparator<Point>() {
+
+			@Override
+			public int compare(Point lhs, Point rhs) {
+				return lhs.x > rhs.x ? 1 : lhs.x == rhs.x ? 0 : -1;
+			}
+		});
+
+		SortedSet<Integer> si = new TreeSet<Integer>();
+		for (Point p : values) {
+			si.add(p.x);
 		}
+
+		int width = (int) (LEFT_PADDING * 2 * mDensity + (si.last() - si
+				.first()) * X_PADDING * mDensity);
+		Log.d("adb width", "" + width);
 	}
 }
---
+++ ADB/ADB1/src/by/bsuir/histogram/HistogramsConstants.java
@@ -0,0 +1,7 @@
+package by.bsuir.histogram;
+
+public interface HistogramsConstants {
+	static final int LEFT_PADDING = 50;
+	static final int TOP_PADDING = 50;
+	int X_PADDING = 5;
+}
