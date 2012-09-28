55009c744aa1cc783927157f614144f71b436c7a
AlexeiVitenko@gmail.com <AlexeiVitenko@gmail.com@8d78331f-9e9b-e9d6-d8e1-e5db5a9c904c>
2012-09-28 07:25:37 +0000

    добавил числа а б р

--- SAMM/SAMM1/src/by/bsuir/samm/LemerRandom.java
+++ SAMM/SAMM1/src/by/bsuir/samm/LemerRandom.java
@@ -1,46 +1,57 @@
 package by.bsuir.samm;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 import javax.swing.SwingWorker;
 
 public class LemerRandom extends SwingWorker<Boolean, Integer> {
 
-	private int a;
-	private int r;
-	private int m;
-	private int goal;
-	private RandomListenet randomListener;
-	private Set<Integer> numbers;
+	private long a;
+	private long r;
+	private long m;
+	private long goal;
+	private Set<Long> numbers = new TreeSet<Long>();
+	private Set<Float> result = new TreeSet<Float>();
+	private StringBuilder sb = new StringBuilder();
 
-	public LemerRandom(int a, int r, int m, int goal, RandomListenet pl) {
+	public LemerRandom(long a, long r, long m, long goal) {
 		this.a = a;
 		this.r = r;
 		this.m = m;
 		this.goal = goal;
-		this.randomListener = pl;
 	}
-
-	public interface RandomListenet {
-		void onProgress();
-
-		void onFail();
-	}
-
+	
+	public StringBuilder getSb() {
+        return sb;
+    }
+	
+	public Set<Float> getResult() {
+        return result;
+    }
+	
 	@Override
 	/**
 	 * @return @true - success, false - otherwise. 
 	 */
 	protected Boolean doInBackground() throws Exception {
-		int nextR = r;
+		long nextR = r;
 		do {
 			nextR = (a*nextR) % m;
 			if (!numbers.add(nextR)) {
 				return false;
 			}
-			setProgress(numbers.size());
+			setProgress((int)(numbers.size()/(float)goal*100));
+			float res = (float)nextR/m;
+			result.add(res);
+			sb.append(res + "\n");
 		} while (numbers.size() < goal);
 		return true;
 	}
-
+	
+	@Override
+	protected void done() {
+	    // TODO Auto-generated method stub
+	    super.done();
+	}
 }
