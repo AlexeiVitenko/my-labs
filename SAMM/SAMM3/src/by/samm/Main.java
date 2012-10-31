ebb0e66b1ccf0fca75753ee54db1960f16166b9d
AlexeiVitenko@gmail.com <AlexeiVitenko@gmail.com@8d78331f-9e9b-e9d6-d8e1-e5db5a9c904c>
2012-10-31 19:21:46 +0000

    Добавил параметры для расчёта.

--- SAMM/SAMM3/src/by/samm/Main.java
+++ SAMM/SAMM3/src/by/samm/Main.java
@@ -7,10 +7,7 @@ public class Main {
      */
     public static void main(String[] args) {
         QueueingModel qm = new QueueingModel();
-        for (int i = 0; i < 10000; i++) {
-            qm.nextMove();
-        }
-        qm.showResults();
+        qm.start(10000);
     }
 
 }
